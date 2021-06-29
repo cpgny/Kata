f13c497633fd185831eba966c4c3b28a1a19b0b8
cpgny <86189916+cpgny@users.noreply.github.com>
2021-06-29 01:39:53 -0500

    Update sol.cpp

--- Twice Linear/sol.cpp
+++ Twice Linear/sol.cpp
@@ -1,1 +1,18 @@
+#include <algorithm>
+#define rep(i, j) for(int i = 1; i <= j; ++i)
 
+class DoubleLinear
+{
+public:
+    static int dblLinear(int n) {
+      int pre = 0, pos = 0;
+      std::vector<int> pairs(n+1);
+      pairs[0] = 1;
+      rep(i, n) {
+        pairs[i] = std::min(1+pairs[pre]*2, 1+pairs[pos]*3);
+        if(pairs[i] == 2*pairs[pre]+1) pre++;
+        if(pairs[i] == 3*pairs[pos]+1) pos++;
+      }
+      return pairs[n];
+    }
+};
