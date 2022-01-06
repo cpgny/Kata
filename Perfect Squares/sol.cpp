ed47c6ef8512c1b54ffdb72e98fffb8772c01142
cpgny <86189916+cpgny@users.noreply.github.com>
2022-01-06 05:10:16 -0600

    Create sol.cpp

---
+++ Perfect Squares/sol.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool stp(int q){
+    int dp=sqrt(q);
+    return (dp*dp==q);
+}
+int sum_of_squares(int n) {
+  if(stp(n)){
+    return 1;
+  }
+  for(int i=1;i<=(int)sqrt(n);++i){
+    if(stp(n-(i* i))){
+      return 2;
+    }
+  }
+  while(n%4==0){
+    n>>=2;
+  }
+  if(n%8==7){
+    return 4;
+  }
+  return 3;
+}
