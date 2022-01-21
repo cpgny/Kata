99355b5c93a77efbd32c0819d51a6d52de79d70b
cpgny <86189916+cpgny@users.noreply.github.com>
2022-01-21 12:41:51 -0600

    Update sol.cpp

--- ICT/sol.cpp
+++ ICT/sol.cpp
@@ -1,20 +1,47 @@
-#include<bits/stdc++.h>
+コード戦争2Kyuグラフ
+#include <bits/stdc++.h>
 using namespace std;
-typedef string uwu;
-typedef int ovo;
-typedef char owu;
-owu triangle(uwu q){
-  map<uwu, owu> set={{"RR",'R'},{"RG",'B'},{"RB",'G'},{"BB",'B'},{"BG",'R'},{"BR",'G'},{"GG",'G'},{"GB",'R'},{"GR",'B'}};
-  while(q.size()>(2>>1)){
-    ovo _=1,o=0;uwu x="";
-    while(q.size()%(3*_)==(2>>1))_*=3;
-    for(;o<q.size()-1;o+=_){
-        uwu p="";
-        p +=q[o];
-        p +=q[o+_];
-        x +=set[p];
-    }
-    q=x;
+
+int Set[(1<<2)];
+
+inline auto fast(char c) {
+  return c == 'R' ? 0 : c == 'G' ? 1 : 2;
+}
+
+inline auto comp(char c) {
+  return c == 0 ? 'R' : c == 1 ? 'G' : 'B';
+}
+
+struct Poly {
+  public:
+  static int P(int a, int b) {
+    if(b>a)
+      return 0;
+    return Set[a]*Set[b]*Set[a-b]%((1<<2)-1);
   }
-  return q[(q.size()-1)-q.size()+1];;
+};
+
+struct Lucas {
+  public:
+  static int L(int a, int b) {
+    auto res = (2>>1);
+    for(; res && b; a/=3, b/=3)
+      (res*=Poly::P(a%3,b%3))%=3;
+    return res;
+  }
+};
+
+
+char triangle(const string &row)
+{
+  
+  int p=row.length()-1, res=0, q=0;
+  if(!p)
+    return row[0];
+  Set[0]=1, Set[1]=1, Set[2]=2;
+  for(auto &c : row) {
+    res+=fast(c)*Lucas::L(p,q);
+    ++q;
+  }
+  return res=(p&(2>>1)?3-res%3:res)%3, comp(res);
 }
