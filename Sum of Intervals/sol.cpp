0d692a0f56687de51f01d6fa9042d897aa8c0bc7
cpgny <86189916+cpgny@users.noreply.github.com>
2022-02-19 10:46:26 +0400

    Create sol.cpp

---
+++ Sum of Intervals/sol.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sum_intervals(vector<pair<int, int>> intervals) {
+  int ans = 0;
+  sort(intervals.begin(), intervals.end());
+  intervals.push_back(make_pair(0, 0));
+  for(int i = 0; i < (int) intervals.size(); ++i) {
+    if(intervals[i].first <= intervals[i + 1].first && 
+       intervals[i].second >= intervals[i + 1].first && 
+       intervals[i].second <= intervals[i + 1].second) {
+       intervals[i + 1].first = intervals[i].first;
+    }
+    else if(intervals[i].first <= intervals[i + 1].first && intervals[i].second >= intervals[i + 1].first
+            && intervals[i].second >= intervals[i + 1].second) {
+      intervals[i + 1].first = intervals[i].first, intervals[i + 1].second = intervals[i].second;
+       }
+    }
+    intervals.push_back(make_pair(0, 0));
+    for(int i = 0; i < (int) intervals.size(); ++i) {
+       if(intervals[i].first != intervals[i + 1].first) {
+           ans += abs(intervals[i].first - intervals[i].second);
+       }
+    }
+    return ans; 
+}
+
